681bf3b8ad3b33f9d1f965d4aa33e5c7cbdc870d
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-06 09:42:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 // Add your functions here
+function mapToNegativize(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(-1 * src[i]) // Unique work
+  }
+  return r
+}
+ 
+function mapToNoChange(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(src[i]) // Unique work
+  }
+  return r
+}
+ 
+function mapToDouble(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(2 * src[i]) // Unique work
+  }
+  return r
+}
+ 
+function mapToSquare(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(src[i] * src[i]) // Unique work
+  }
+  return r
+}
 
 function map(array, f) {
-  f
+  return f
 }
