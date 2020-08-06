f171ad0de7054dc6b5cba84c33bcf0b15aeb4191
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-06 10:08:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Add your functions here
+
+
+function map(array, f) {
+  return array.map(f)
+}
+
+function reduce(array, f, startingPoint=0) {
+  if (startingPoint===0) {
+    return array.reduce(f)
+  } else {
+    return array.reduce(f, startingPoint)
+  }
+}
